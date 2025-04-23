e609d841fda3b430df1c20bc55eff975cff913ab
D3HUX <nik2015za@gmail.com>
2025-04-23 19:48:52 +0300

    Added cursor from assets, added resolution choice using console

--- demo 1.py
+++ demo 1.py
@@ -1,18 +1,37 @@
 import pygame
+from assets import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280, 720))
 pygame.mouse.set_visible(False)
+cursor = pygame.image.load(os.path.join('assets', 'cursor.png'))
+cursor_rect = cursor.get_rect()
+
 clock = pygame.time.Clock()
+
+resolutions = {1:(1280, 720), 2:(1920, 1080), 3:(2560, 1440)}
+resolution_choice = int(input("Choose your resolution:\n1. 1280 x 720\n2. 1920 x 1080\n3. 2560 x 1440\n"))
+if resolution_choice not in resolutions.keys():
+    exit()
+resolution = resolutions[resolution_choice]
+
+#tickrates = (30, 60, 120, 144, 240)
+#tick_choice = int(input("Choose your tickrate:\n1. 30\n2. 60\n3. 120\n4. 144\n5. 240\n"))
+#if tick_choice not in tickrates:
+#    exit()
+#ticks = int(tickrates[tick_choice - 1])        WIP kood
+
+screen = pygame.display.set_mode((resolution[0], resolution[1]))
 
 running = True
 while running:
     screen.fill((0, 0, 0))
-    circle = pygame.draw.circle(screen, (255, 255, 255), pygame.mouse.get_pos(), 10)
+    cursor_rect.center = pygame.mouse.get_pos()
+    screen.blit(cursor, cursor_rect.center)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(60)
     pygame.display.flip()
