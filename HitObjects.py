b0b4077836df4a92d1feae87c3bd9ff8879ad231
D3HUX <nik2015za@gmail.com>
2025-05-08 01:04:22 +0300

    working asset usage on older commit

--- HitObjects.py
+++ HitObjects.py
@@ -1,6 +1,7 @@
 """OOP representations of hit objects (hit circles, sliders, spinners)."""
 
 import pygame
+import os
 
 class HitCircle:
     def __init__(self, position: tuple[int, int], time, combo_num):
@@ -10,12 +11,14 @@ class HitCircle:
         self.y = self.position[1]
         self.combo_num = combo_num
 
-    def draw(self, screen: pygame.surface, size, progress):
+    def draw(self, screen: pygame.surface, size, progress, ):
         """
         Draw hit circle on screen.
 
         Progress is a float that determines the radius of the approach circle (0: fully outside, 1: flush with circle)"""
-        pygame.draw.circle(screen, (255, 255, 255), self.position, size)
+        hitcircle = pygame.image.load(os.path.join('assets', 'circle_pls-work.png'))
+        hitcircle = pygame.transform.scale(hitcircle, (size, size))
+        screen.blit(hitcircle, (self.position[0] - hitcircle.get_height() / 2, self.position[1] - hitcircle.get_width() / 2))
         # Draw approach circle
         approach_radius = size * 2 - (progress * size)
         pygame.draw.circle(screen, (255, 255, 255), self.position, approach_radius, width=3)
